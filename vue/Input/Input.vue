efcaf5eaf189c1693d6a5e3792379224b15e4230
tinway <76844040+tinway@users.noreply.github.com>
2025-07-13 03:13:05 +0300

    commit

---
+++ vue/Input/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <input :class="finalClass" v-bind="$attrs" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({ class: String });
+const finalClass = computed(() =>
+    `px-3 py-2 border border-gray-300 rounded outline-none focus:ring-2 focus:ring-blue-500 ${props.class || ''}`
+);
+</script>
