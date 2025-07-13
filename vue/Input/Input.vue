13aad52b8728a8185d85aaa3e9cc164eceea5c26
tinway <76844040+tinway@users.noreply.github.com>
2025-07-13 04:24:46 +0300

    commit

--- vue/Input/Input.vue
+++ vue/Input/Input.vue
@@ -1,12 +1,17 @@
 <template>
-    <input :class="finalClass" v-bind="$attrs" />
+  <input class="input" v-bind="$attrs" />
 </template>
 
-<script setup>
-import { computed } from 'vue';
-
-const props = defineProps({ class: String });
-const finalClass = computed(() =>
-    `px-3 py-2 border border-gray-300 rounded outline-none focus:ring-2 focus:ring-blue-500 ${props.class || ''}`
-);
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: true
+});
 </script>
+
+<style scoped>
+.input {
+  padding: 8px 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
